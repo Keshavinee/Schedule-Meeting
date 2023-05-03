1a1e9f7f99b77c8d13c99a3bb358a47a5a38e775
Keshavinee <shrikeshavinee@gmail.com>
2023-05-03 10:12:56 +0530

    Backend for login & signup

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
+import hashlib
+
+app = Flask(__name__)
+app.secret_key = 'keshav@ssn'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Check if the user is trying to log in
+        if "lmail" in request.form:
+            email = request.form.get('lmail')
+            password = request.form['pass']
+            # Hash the password before comparing it to the stored hash
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            conn = sqlite3.connect('nbyula_terraformers.db')
+            c = conn.cursor()
+            c.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, hashed_password))
+            user = c.fetchone()
+            conn.close()
+            if user:
+                # Store the username in the session
+                session['user'] = user[0]
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid username or password'
+                return render_template('index.html', error=error)
+            
+        # Check if the user is trying to sign up
+        elif 'uname' in request.form:
+            username = request.form['uname']
+            email = request.form['mail']
+            password = request.form['pwd']
+            # Hash the password before storing it in the database
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            conn = sqlite3.connect('nbyula_terraformers.db')
+            c = conn.cursor()
+            c.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, hashed_password))
+            user = c.fetchone()
+            print(user)
+            if user:
+                error = 'Username already taken'
+                conn.close()
+                return render_template('index.html', error=error) 
+            
+            else:
+                c.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (username, email, hashed_password))
+                conn.commit()
+                conn.close()
+                return redirect(url_for('index'))
+    else:
+        return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    # Check if the user is logged in
+    if 'user' in session:
+        conn = sqlite3.connect('nbyula_terraformers.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM users WHERE username = ?", (session['user'],))
+        user = c.fetchone()
+        conn.close()
+        return render_template('dashboard.html', user=user)
+    else:
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
